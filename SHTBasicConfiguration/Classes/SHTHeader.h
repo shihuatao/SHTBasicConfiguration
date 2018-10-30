c87d2751d8466c00c26c75e249080097989f8c02
steve <shiht@mapbar.com>
2018-10-30 17:45:31 +0800

    update

--- SHTBasicConfiguration/Classes/SHTHeader.h
+++ SHTBasicConfiguration/Classes/SHTHeader.h
@@ -8,5 +8,18 @@
 #ifndef SHTHeader_h
 #define SHTHeader_h
 
+/** 设备信息 */
+#define SizeWidth    ([[UIScreen mainScreen] bounds].size.width)
+#define SizeHeight   ([[UIScreen mainScreen] bounds].size.height)
+
+#define kIphone6Width(w) (((w) * SizeWidth) / 375.0f)
+#define kIphone6Height(h) (((h) * SizeHeight) / 667.0f)
+#define kNavigationHeight (44 + [[UIApplication sharedApplication] statusBarFrame].size.height)
+#define kStatusBarHeight ([[UIApplication sharedApplication] statusBarFrame].size.height)
+
+//#define kCurrentAppVersion ([[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"])
+//#define kCurrentSystemVersion [[[UIDevice currentDevice] systemVersion] floatValue]
+//#define kCurrentDeviceVersion [UIDevice currentDevice].systemVersion
+
 
 #endif /* SHTHeader_h */
